5f144fca762932200ca09cff62d96557e3cf2c1e
Lisa LaRochelle <larochelle.lisa@gmail.com>
2019-07-09 16:23:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,12 +8,3 @@ end
   
 reverse_each_word("Hi you")
   
-  
-def reverse_each_word(string)
-  array = string.split(" ")
-  rev_array = []
-  array.each {|words| rev_array << words.reverse}
-  rev_array.join(" ")
-end
-
-reverse_each_word("Hi you")

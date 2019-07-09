939dc0619d29e2055ea8c1b772b2ac61b4512c2a
Lisa LaRochelle <larochelle.lisa@gmail.com>
2019-07-09 16:20:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,7 @@ def reverse_each_word(string)
   array = string.split(" ")
   array.collect {|words| words.reverse}
 end
+array.join(" ")
   
 reverse_each_word("Hi you")
   
@@ -11,7 +12,6 @@ def reverse_each_word(string)
   rev_array = []
   array.each {|words| rev_array << words.reverse}
   rev_array.join(" ")
- 
 end
 
 reverse_each_word("Hi you")
